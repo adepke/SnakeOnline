3694dfbc1f294ca95b2ad4d34b5104a7e386c0f1
Andrew Depke <andrewdepke@gmail.com>
2018-02-09 23:04:23 -0700

    Restructured Solution

--- SnakeOnline/GameView.cs
+++ SnakeOnline/GameView.cs
@@ -6,5 +6,6 @@ namespace SnakeOnline
 {
     class GameView
     {
+        public World WorldInst;
     }
 }
--- SnakeOnline/Program.cs
+++ SnakeOnline/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 
+using OpenTK;
+
 namespace SnakeOnline
 {
     class Program
     {
-        
+        public static void Main()
+        {
+            GameWindow AppWindow = new GameWindow(800, 600, OpenTK.Graphics.GraphicsMode.Default, "Snake Online", 0, DisplayDevice.Default);
+
+            AppWindow.Run(30.0d);
+        }
     }
 }
