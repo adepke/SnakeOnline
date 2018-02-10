1c7f62f320ecf3a8bbb9a6bc685a63b7f3aea37a
Andrew Depke <andrewdepke@gmail.com>
2018-02-09 22:11:54 -0700

    World Class Base

---
+++ SnakeOnline/World.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SnakeOnline
+{
+    class World
+    {
+        private int Rows;
+        private int Columns;
+
+        private object[,] ItemMatrix;
+
+        public bool Initialize(int Rows, int Columns)
+        {
+            ItemMatrix = new object[Rows, Columns];
+
+            this.Rows = Rows;
+            this.Columns = Columns;
+
+            return true;
+        }
+
+        public int GetRows()
+        {
+            return Rows;
+        }
+
+        public int GetColumns()
+        {
+            return Columns;
+        }
+
+        private bool IsValidIndex(int Row, int Column)
+        {
+            return (Row <= Rows && Column <= Columns);
+        }
+
+        public object Get(int Row, int Column)
+        {
+            if (!IsValidIndex(Row, Column))
+                return default(object);
+
+            return ItemMatrix[Row, Column];
+        }
+
+        public bool Set(object Object, int Row, int Column)
+        {
+            if (!IsValidIndex(Row, Column))
+                return false;
+
+            ItemMatrix[Row, Column] = Object;
+
+            return true;
+        }
+    }
+}
