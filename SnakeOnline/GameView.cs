1f016f1e7f503744f5079a1293a0595798286165
Andrew Depke <andrewdepke@gmail.com>
2018-02-10 00:39:35 -0700

    Added Snake to Game View

--- SnakeOnline/GameView.cs
+++ SnakeOnline/GameView.cs
@@ -7,12 +7,20 @@ namespace SnakeOnline
     class GameView
     {
         public World WorldInst;
+        public Snake SnakeInst;
 
         public bool Initialize(int WorldSizeX, int WorldSizeY)
         {
             WorldInst = new World();
 
             if (!WorldInst.Initialize(WorldSizeX, WorldSizeY))
+            {
+                return false;
+            }
+
+            SnakeInst = new Snake();
+
+            if (!SnakeInst.Initialize(WorldInst))
             {
                 return false;
             }
