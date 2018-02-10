8ee2992c18660c12f1da2e88f30dbcdb62620c15
Andrew Depke <andrewdepke@gmail.com>
2018-02-09 21:56:21 -0700

    Initial Layout

--- SnakeOnline/Program.cs
+++ SnakeOnline/Program.cs
@@ -4,9 +4,6 @@ namespace SnakeOnline
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        
     }
 }
---
+++ SnakeOnlineServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeOnlineServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
