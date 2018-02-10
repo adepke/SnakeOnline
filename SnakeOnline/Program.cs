58adaf849d9c59dd8baa94c9104dd79ce6c79c08
Andrew Depke <andrewdepke@gmail.com>
2018-02-09 21:46:20 -0700

    Add project files.

---
+++ SnakeOnline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
