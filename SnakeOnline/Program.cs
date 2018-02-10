a96df8de629015dabac53f7607dbc72120e27813
Andrew Depke <andrewdepke@gmail.com>
2018-02-10 01:03:36 -0700

    Fixed Missing Parameter Error

--- SnakeOnline/Program.cs
+++ SnakeOnline/Program.cs
@@ -11,7 +11,7 @@ namespace SnakeOnline
             GameWindow AppWindow = new GameWindow(800, 600, OpenTK.Graphics.GraphicsMode.Default, "Snake Online", 0, DisplayDevice.Default);
 
             GameView LocalView = new GameView();
-            if (!LocalView.Initialize(5, 5))
+            if (!LocalView.Initialize(AppWindow, 5, 5))
             {
                 throw new Exception("Failed to Create Local Game View");
             }
