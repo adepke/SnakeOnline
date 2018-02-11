13897f63f44d2883f026bfc58bb4f7f730fa60ca
Andrew Depke <andrewdepke@gmail.com>
2018-02-11 14:45:03 -0700

    Setup for Better Item Spawning

--- SnakeOnline/LocalItemSpawner.cs
+++ SnakeOnline/LocalItemSpawner.cs
@@ -24,7 +24,50 @@ namespace SnakeOnline
             int Row = RandomHandler.Next(0, WorldInst.GetRows());
             int Column = RandomHandler.Next(0, WorldInst.GetColumns());
 
-            WorldInst.Set(2, Row, Column);
+            if ((int)WorldInst.Get(Row, Column) == 1)
+            {
+                // Find Closet Open Cell
+
+                int Iter = 0;
+
+                while (Iter != -1)
+                {
+                    List<Point> Shell = SurroundingCells(Row, Column, Iter);
+
+                    // No Open Cells in the World, Don't Spawn.
+                    if (Shell.Count == 0)
+                    {
+                        break;
+                    }
+
+                    foreach (Point NewPoint in Shell)
+                    {
+                        // If this Point is Open
+                        if ((int)WorldInst.Get(NewPoint.Row, NewPoint.Column) == 0)
+                        {
+                            Iter = -2;
+
+                            WorldInst.Set(2, NewPoint.Row, NewPoint.Column);
+
+                            break;
+                        }
+                    }
+
+                    ++Iter;
+                }
+            }
+
+            else
+            {
+                WorldInst.Set(2, Row, Column);
+            }
+        }
+
+        private List<Point> SurroundingCells(int Row, int Column, int Gap)
+        {
+            List<Point> Result = new List<Point>();
+
+            return Result;
         }
     }
 }
